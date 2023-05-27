4fb88703bbde51e1fc3a37e72324ea57dc633120
IvanVenerov <ivanalipov4@gmail.com>
2023-05-27 02:54:07 +0200

    second commit

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 function scrollToElement(event, targetElement) {
   event.preventDefault(); // Отменяем стандартное действие ссылки
-  
+
   const element = document.getElementById(targetElement); // Получаем элемент, к которому нужно прокрутить
-  
+
   if (element) {
-    element.scrollIntoView({ behavior: 'smooth' }); // Прокручиваем до элемента с плавной анимацией
+    element.scrollIntoView({ behavior: "smooth" }); // Прокручиваем до элемента с плавной анимацией
   }
 }
 
 function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: 'smooth' }); // Прокручиваем наверх страницы с плавной анимацией
+  window.scrollTo({ top: 0, behavior: "smooth" }); // Прокручиваем наверх страницы с плавной анимацией
 }
 function redirectToPage1() {
-      window.location.href = "softdownload.html";
-    }
-
-    function redirectToPage2() {
-      window.location.href = "softscreanshot.html";
+  window.location.href = "index_2.html";
 }
-    function redirectToPage3() {
-      window.location.href = "techdownload.html";
-    }
 
-    function redirectToPage4() {
-      window.location.href = "techscreanshot.html";
-    }
+function redirectToPage2() {
+  window.location.href = "index_3.html";
+}
+function redirectToPage3() {
+  window.location.href = "index_4.html";
+}
+
+function redirectToPage4() {
+  window.location.href = "index_5.html";
+}
