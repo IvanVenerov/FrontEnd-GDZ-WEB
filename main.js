c264056574177bf34be682e1dc8ed623eac1d32c
IvanVenerov <ivanalipov4@gmail.com>
2023-05-31 22:31:45 +0200

    fuuh

---
+++ main.js
@@ -0,0 +1,9 @@
+const agreementCheckbox = document.getElementById('agreementCheckbox');
+const myLink = document.getElementById('myLink');
+
+myLink.addEventListener('click', function(event) {
+  if (!agreementCheckbox.checked) {
+    event.preventDefault();
+    alert('Вы не погодились з користувальницькою угодою !');
+  }
+});
