b0856215d958783d4fa71ac81053d7f221eeed7a
IvanVenerov <ivanalipov4@gmail.com>
2023-06-01 15:06:09 +0200

    commiiiiit
    312321321

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ const myLink = document.getElementById("myLink");
 myLink.addEventListener("click", function (event) {
   if (!agreementCheckbox.checked) {
     event.preventDefault();
-    alert("Вы не погодились з користувальницькою угодою !");
+    alert("Ви не погодились з користувальницькою угодою !");
   }
 });
